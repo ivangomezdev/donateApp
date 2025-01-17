4063ffc51fd4dd3136f4a50ee91755a6a855144c
ivan <ivansangomez6@gmail.com>
2025-01-17 11:57:25 -0500

    v4

--- src/app/payment/status/page.tsx
+++ src/app/payment/status/page.tsx
@@ -1,22 +1,35 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import React, { useEffect } from "react";
+
 const PaymentStatusPage = () => {
   useEffect(() => {
     const fetchPaymentStatus = async () => {
-      const response = await fetch("/api/payments/status");
-      const data = await response;
-      console.log(data);
-      
-      // Aquí podrías actualizar la UI basada en la respuesta
+      try {
+        const response = await fetch("/api/payments/status");
+        
+        // Asegúrate de convertir la respuesta en JSON
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        const data = await response.json(); // Aquí obtienes el cuerpo de la respuesta en JSON
+        console.log(data);
+
+        // Aquí podrías actualizar la UI basada en la respuesta
+      } catch (error) {
+        console.error("Error fetching payment status:", error);
+      }
     };
+
     fetchPaymentStatus();
   }, []);
 
   return (
     <div>
       {/* Aquí podrías mostrar el estado del pago basado en 'data' */}
+      <h1>Verificando estado del pago...</h1>
     </div>
-  )
-}
+  );
+};
 
 export default PaymentStatusPage;
--- src/pages/api/payments/status/index.ts
+++ src/pages/api/payments/status/index.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const payload = req.body;
-    console.log(payload);
+    console.log(payload.json());
 
     return res.status(200).json({ ok: true });
   }
